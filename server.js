41c8c25b59488033158b564a258c6f20ec8733cc
heekymusic <151211744+heekymusic@users.noreply.github.com>
2026-08-31 19:23:43 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Portal de Exames funcionando!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
