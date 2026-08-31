c1da97b93e34109e0b558a97e3c41c6501cc2997
heekymusic <151211744+heekymusic@users.noreply.github.com>
2026-08-31 19:51:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
